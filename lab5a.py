d16727b2616bbe5e82a2173ec3cd56fe2ea925ad
Thrishik_Shetty_17 <117003353+ThrishikShetty@users.noreply.github.com>
2023-08-28 22:19:07 +0530

    Update lab5a.py

--- lab5a.py
+++ lab5a.py
@@ -8,17 +8,17 @@ def isphonenumber(phonenumber):
     if len(phonenumber)!= 12:
         return False
     for i in range(0,3):
-        if not phonenumber[i].isdecimal():
+        if phonenumber[i].isdecimal()== False:
             return False
     if phonenumber[3]!='-':
         return False
     for i in range(4,7):
-         if not phonenumber[i].isdecimal():
+         if phonenumber[i].isdecimal()== False:
             return False
     if phonenumber[7]!='-':
         return False
     for i in range(8,12):
-        if not phonenumber[i].isdecimal():
+        if phonenumber[i].isdecimal()== False:
             return False
     return True
 
